005c920c8c0c3951fe2892a4797deac5c658c213
conradstoerker <conrad.stoerker@progenity.com>
2015-06-09 10:21:44 -0400

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,27 +1,35 @@
 #!/bin/bash
 
-FILEPATH='/home/conrad/fastQC_docker/fastq_files/'
-if [ -d "$1"]; then
-	FILEPATH="$1"
-fi
-	
+#FILEPATH='/home/conrad/fastQC_docker/fastq_files/'
+#if [ -d $1]; then
+#	FILEPATH=$1
+#fi
+
+#the first installs if you don't have it, the second will update your current docker
+echo Making sure you have the latest docker package...
+#wget -qO- https://get.docker.com/ | sh
+wget -N https://get.docker.com/ | sh
+
+
 echo Restarting docker...  
-sudo service docker stop  
-sudo service docker start
+service docker stop  
+service docker start
 
 #Must remove a preexisting container with the name we are going to use
-#sudo docker rm -f fastqc
+docker rm -f fastqc
 
 echo pulling conradstoerker/fastqc...
-sudo docker pull conradstoerker/fastqc
+docker pull conradstoerker/fastqc
 
+#The path should be changed to whatever directory contains the fastq files
 echo running conradstoerker/fastqc...
-sudo docker run --name fastqc -v $FILEPATH:/fastqc/data conradstoerker/fastqc
+#docker run --name fastqc -v $FILEPATH:/fastqc/data conradstoerker/fastqc
+docker run --name fastqc -v '/home/conrad/fastQC_docker/fastq_files/':/fastqc/data conradstoerker/fastqc
 
 #echo fastqc version:
 #fastqc -v
 
 echo exiting...
-sudo docker rm fastqc
+docker rm -f fastqc
 #exit
  

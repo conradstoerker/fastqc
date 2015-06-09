2605c57e545117fac56bc72412fb9e85031dc0e1
conradstoerker <conrad.stoerker@progenity.com>
2015-06-09 08:33:33 -0400

    Update automate.sh

--- automate.sh
+++ automate.sh
@@ -1,15 +1,21 @@
+#!/bin/bash
+
 echo Restarting docker...  
 sudo service docker stop  
 sudo service docker start
+
+#Must remove a preexisting container with the name we are going to use
+sudo docker rm -f fastqc
 
 echo pulling conradstoerker/fastqc...
 sudo docker pull conradstoerker/fastqc
 
 echo running conradstoerker/fastqc...
-docker run --name fastqc -i -v '/home/conrad/fastQC_docker/fastq_files/':/fastqc/data conradstoerker/fastqc
+sudo docker run --name fastqc -i -v '/home/conrad/fastQC_docker/fastq_files/':/fastqc/data conradstoerker/fastqc
 
-echo fastqc version:
-fastqc -v
+#echo fastqc version:
+#fastqc -v
 
 echo exiting...
-exit
+sudo docker rm -f fastqc
+#exit

d16c19e0bfbc4bcb15179772b3f615929baa7297
conradstoerker <conrad.stoerker@progenity.com>
2015-06-19 11:32:05 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-#Assumes there is a file with the number of kmers to use in the same directory as the fastq files
-mers="/fastqc/data/kmers.txt"
-KMERS=$(egrep -o '[2-9]+' $mers | egrep -c '^1')
-
 for file in /fastqc/data/*.fastq
 do
-	fastqc -k $KMERS $file
+	fastqc $file
 done
 
 for file in /fastqc/data/*.fastq.gz
 do
-	fastqc -k $KMERS $file
+	fastqc $file
 done
 
 exit
